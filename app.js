6073c641fb39858b7c5052213d8db6ee67a29829
moizawan07 <moizahmedawan321@gmail.com>
2024-09-21 13:04:54 -0700

    loop-assignment

---
+++ app.js
@@ -0,0 +1,20 @@
+var user = prompt("Enter a  number");
+
+for(var i =1; i<=user; i++){
+    
+    for(var j = 1; j<=i; j++){
+        document.write(j + "  ")
+    }; 
+    document.write("<br>")
+};
+document.write("<br>")
+var user2 = prompt("enter a ");
+
+for(var k = 1; k<=user2; k++){
+
+    for(var l = 1; l<=k; l++){
+    document.write( "*" + "  ")
+    }; 
+    document.write("<br>")
+};
+
